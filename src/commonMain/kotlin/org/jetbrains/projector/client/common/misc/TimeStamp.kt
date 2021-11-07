f55f86bd75629ed55283475fa193de2bf32b711a
soywiz <soywiz@gmail.com>
2021-11-07 01:45:33 +0100

    Fixes TimeStamp.current

--- src/commonMain/kotlin/org/jetbrains/projector/client/common/misc/TimeStamp.kt
+++ src/commonMain/kotlin/org/jetbrains/projector/client/common/misc/TimeStamp.kt
@@ -3,5 +3,5 @@ package org.jetbrains.projector.client.common.misc
 import com.soywiz.klock.DateTime
 
 object TimeStamp {
-  val current: Double = DateTime.nowUnix()
+  val current: Double get() = DateTime.nowUnix()
 }
